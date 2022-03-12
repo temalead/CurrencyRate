87a7658c78ceb6e68c7ad00e550d5864d0aa9173
nikulin-me <itdetectiv@gmail.com>
2022-03-12 22:02:01 +0400

    Repaired necessary dependencies

---
+++ rate/src/main/java/cbr/RateRunner.java
@@ -0,0 +1,11 @@
+package cbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(RateRunner.class,args);
+    }
+}
--- rate/src/main/java/cbr/config/ApplicationConfig.java
+++ rate/src/main/java/cbr/config/ApplicationConfig.java
@@ -2,12 +2,12 @@ package cbr.config;
 
 import cbr.model.CachedCurrencyRates;
 import org.ehcache.Cache;
+import org.ehcache.CacheManager;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
